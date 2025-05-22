3e150e1d6c3ed1ea85daec3deb9bdede8da88f7d
SEDHU BABU <135299639+Sedhubabu@users.noreply.github.com>
2025-05-22 11:05:41 +0530

    Update settings.py

--- a_core/settings.py
+++ a_core/settings.py
@@ -155,9 +155,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+import os
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [ BASE_DIR / 'static' ]
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # or any folder name you want
+STATIC_URL = '/static/'
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media' 
